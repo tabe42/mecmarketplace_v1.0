36326a70a30b4a487b6413574872e0e4d73d29cd
Devanarayan J <devan23.5.2001@gmail.com>
2022-05-14 22:48:02 +0530

    remade item component

--- src/components/ItemBar.js
+++ src/components/ItemBar.js
@@ -15,29 +15,26 @@ export const ItemBar = ({
   productsellerid,
 }) => {
   return (
-    <div className=" flex flex-col space-y-4 md:flex-row justify-between items-center md:space-x-5  bg-blue-600 text-white  p-4 rounded-md shadow-lg w-3/4 md:w-1/2 relative">
+    <div className=" flex flex-col space-y-4 md:space-y-0 md:flex-row justify-start items-center   bg-blue-600 text-white  p-4 rounded-md shadow-lg w-3/4 md:w-1/2 ">
       <img src={imageurl} alt="error" className="h-1/3 md:w-1/3  " />
-      <div className="flex flex-col">
+      <div className="flex flex-col pl-4">
         <p>{productname}</p>
         <p>Seller: {name}</p>
         <p>Contact: {contact}</p>
         <p>Price: {price}</p>
         <p>{description}</p>
       </div>
-      <div>
-        {isAuth && productsellerid === auth.currentUser.uid && (
-          <button
-            onClick={() => {
-              deleteProduct(id);
-            }}
-            className="bg-white p-2 rounded-md self-start text-blue-600 hover:bg-gray-200"
-          >
-            Delete
-          </button>
-        )}
-      </div>
 
-      {/* <button className="bg-red-600 p-2 absolute top-0 right-0">delete</button> */}
+      {isAuth && productsellerid === auth.currentUser.uid && (
+        <button
+          onClick={() => {
+            deleteProduct(id);
+          }}
+          className="bg-white p-2 rounded-md  text-blue-600 hover:bg-gray-200 ml-auto self-end "
+        >
+          Delete
+        </button>
+      )}
     </div>
   );
 };
